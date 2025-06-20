d2c29df9d42ce76b626a0505018ec59faa1bccae
Harry Glen <harrison.glen@student.adelaide.edu.au>
2025-06-20 02:42:11 +0000

    [automatic commit] 2025-06-20 02:42:11 UTC 103.237.18.6

--- part1/question5.sql
+++ part1/question5.sql
@@ -39,5 +39,7 @@ VALUES (
 -- 5 walk requests
 INSERT INTO WalkRequests(dog_id, requested_time, duration_minutes, location, status)
 VALUES(
-    (SELECT dog_id, f)
+    (SELECT dog_id, FROM Dogs WHERE name='name')
+    'datetime',
+    
     )
