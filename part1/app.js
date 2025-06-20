7ada02922adea619e6ac4a8ff93cc4c9821bc2b8
Harry Glen <harrison.glen@student.adelaide.edu.au>
2025-06-20 03:47:34 +0000

    [automatic commit] 2025-06-20 03:47:34 UTC 103.237.18.6

--- part1/app.js
+++ part1/app.js
@@ -10,6 +10,17 @@ async function main() {
         database: 'DogWalkService'
     });
 
+    await connection.query(`
+        INSERT INTO Users(username, email, password_hash, role)
+        VALUES
+            ('alice123', 'alice@example.com', 'hashed123', 'owner'),
+            ('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
+            ('carol123', 'carol@example.com', 'hashed789', 'owner'),
+            ('harrywalker', 'harry@example.com', 'hased987', 'walker'),
+            ('dave234', 'dave@example.com', 'hased650', 'owner')
+        ON DUPLICATE KEY UPDATE username = username;
+    `);
+
     // seed data
     await connection.query(`
         INSERT INTO Users(username, email, password_hash, role)
