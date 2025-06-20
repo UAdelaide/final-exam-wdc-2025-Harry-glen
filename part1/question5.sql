f0e3a6125375b21b2aa36bdde3cded5153e7414e
Harry Glen <harrison.glen@student.adelaide.edu.au>
2025-06-20 02:45:02 +0000

    [automatic commit] 2025-06-20 02:45:02 UTC 103.237.18.6

--- part1/question5.sql
+++ part1/question5.sql
@@ -2,9 +2,9 @@
 INSERT INTO Users(username, email. password_hash, role)
 VALUES
     ('alice123', 'alice@example.com', 'hashed123', 'owner')
-    ('bobwalker', 'bob@example.com', 'hashpass', 'role')
+    ('bobwalker', 'bob@example.com', 'hashed456', 'walker')
     ('carol123', 'carol@example.com', 'hashed789', 'owner')
-    ('name', 'email', 'hashpass', 'role')
+    ('harrywalker', 'harry@example.com', 'hashpass', 'role')
     ('name', 'email', 'hashpass', 'role')
     ('name', 'email', 'hashpass', 'role')
 
