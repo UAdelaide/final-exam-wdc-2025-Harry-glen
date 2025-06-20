f160d415217d21610766f8e38ff82a2d727ad2ad
Harry Glen <harrison.glen@student.adelaide.edu.au>
2025-06-20 02:45:22 +0000

    [automatic commit] 2025-06-20 02:45:22 UTC 103.237.18.6

--- part1/question5.sql
+++ part1/question5.sql
@@ -4,7 +4,7 @@ VALUES
     ('alice123', 'alice@example.com', 'hashed123', 'owner')
     ('bobwalker', 'bob@example.com', 'hashed456', 'walker')
     ('carol123', 'carol@example.com', 'hashed789', 'owner')
-    ('harrywalker', 'harry@example.com', 'hashpass', 'role')
+    ('harrywalker', 'harry@example.com', 'hased987', 'walker')
     ('name', 'email', 'hashpass', 'role')
     ('name', 'email', 'hashpass', 'role')
 
