492c62f8cd12985a48db1b73954810f40d0bbfb2
Harry Glen <harrison.glen@student.adelaide.edu.au>
2025-06-20 03:21:10 +0000

    [automatic commit] 2025-06-20 03:21:10 UTC 103.237.18.6

--- part1/app.js
+++ part1/app.js
@@ -21,13 +21,32 @@ async function main() {
     `);
 
     await connection.query(`
-        INSERT INTO Users(username, email, password_hash, role)
-        VALUES
-        ('alice123', 'alice@example.com', 'hashed123', 'owner'),
-        ('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
-        ('carol123', 'carol@example.com', 'hashed789', 'owner'),
-        ('harrywalker', 'harry@example.com', 'hased987', 'walker'),
-        ('dave234', 'dave@example.com', 'hased650', 'owner');
+        INSERT INTO Dogs (owner_id, name, size)
+        VALUES (
+            (SELECT user_id FROM Users WHERE username='alice123'),
+            'Max',
+            'medium'
+            ),
+            (
+            (SELECT user_id FROM Users WHERE username='carol123'),
+            'Bella',
+            'small'
+            ),
+            (
+            (SELECT user_id FROM Users WHERE username='dave234'),
+            'Roxy',
+            'medium'
+            ),
+            (
+            (SELECT user_id FROM Users WHERE username='carol123'),
+            'Beast',
+            'small'
+            ),
+            (
+            (SELECT user_id FROM Users WHERE username='dave234'),
+            'Tiny',
+            'large'
+    );
     `);
 
     await connection.query(`
