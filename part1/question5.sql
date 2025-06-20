0ec28ff5629f570780c9eec9fca92dd9faa577b0
Harry Glen <harrison.glen@student.adelaide.edu.au>
2025-06-20 02:43:35 +0000

    [automatic commit] 2025-06-20 02:43:34 UTC 103.237.18.6

--- part1/question5.sql
+++ part1/question5.sql
@@ -39,31 +39,31 @@ VALUES (
 -- 5 walk requests
 INSERT INTO WalkRequests(dog_id, requested_time, duration_minutes, location, status)
 VALUES(
-    (SELECT dog_id FROM Dogs WHERE name='name')
+    (SELECT dog_id FROM Dogs WHERE name='name'),
     'datetime',
     1,
     'location'
     ),
     (
-    (SELECT dog_id, FROM Dogs WHERE name='name')
+    (SELECT dog_id FROM Dogs WHERE name='name'),
     'datetime',
     1,
     'location'
     ),
     (
-        (SELECT dog_id, FROM Dogs WHERE name='name')
+        (SELECT dog_id FROM Dogs WHERE name='name'),
     'datetime',
     1,
     'location'
     ),
     (
-        (SELECT dog_id, FROM Dogs WHERE name='name')
+        (SELECT dog_id FROM Dogs WHERE name='name'),
     'datetime',
     1,
     'location'
     ),
     (
-        (SELECT dog_id, FROM Dogs WHERE name='name')
+        (SELECT dog_id FROM Dogs WHERE name='name'),
     'datetime',
     1,
     'location'
