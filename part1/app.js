d5628187b9c6351e0457c7b2f178ff05f16c6785
Harry Glen <harrison.glen@student.adelaide.edu.au>
2025-06-20 03:18:11 +0000

    [automatic commit] 2025-06-20 03:18:11 UTC 103.237.18.6

--- part1/app.js
+++ part1/app.js
@@ -2,5 +2,7 @@ const express = require('express');
 const mysql = require('mysql2/promise');
 
 async function main() {
-    const co
+    const connection = await mysql.createConnection({
+        host; 'localhost'
+    })
 }
