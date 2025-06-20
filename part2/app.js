c1cdc595a866ed579956974ee1b81900e419215b
Harry Glen <harrison.glen@student.adelaide.edu.au>
2025-06-20 06:27:38 +0000

    [automatic commit] 2025-06-20 06:27:38 UTC ;; communications error to 208.67.222.222#53: timed out 103.237.18.6

--- part2/app.js
+++ part2/app.js
@@ -10,7 +10,7 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Session setup
 app.use(session({
