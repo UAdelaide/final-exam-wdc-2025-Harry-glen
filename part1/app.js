e9304649559a59a8f4c0247510b488ec4d140129
Harry Glen <harrison.glen@student.adelaide.edu.au>
2025-06-20 03:21:31 +0000

    [automatic commit] 2025-06-20 03:21:31 UTC 103.237.18.6

--- part1/app.js
+++ part1/app.js
@@ -50,13 +50,43 @@ async function main() {
     `);
 
     await connection.query(`
-        INSERT INTO Users(username, email, password_hash, role)
-        VALUES
-        ('alice123', 'alice@example.com', 'hashed123', 'owner'),
-        ('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
-        ('carol123', 'carol@example.com', 'hashed789', 'owner'),
-        ('harrywalker', 'harry@example.com', 'hased987', 'walker'),
-        ('dave234', 'dave@example.com', 'hased650', 'owner');
+        INSERT INTO WalkRequests(dog_id, requested_time, duration_minutes, location, status)
+        VALUES(
+            (SELECT dog_id FROM Dogs WHERE name='Max'),
+            '2025-06-10 08:00:00',
+            30,
+            'Parklands',
+            'open'
+            ),
+            (
+            (SELECT dog_id FROM Dogs WHERE name='Bella'),
+            '2025-06-10 09:30:00',
+            45,
+            'Beachside Ave',
+            'accepted'
+            ),
+            (
+                (SELECT dog_id FROM Dogs WHERE name='Roxy'),
+            '2024-06-10 16:30:00',
+            55,
+            'Golden Rd',
+            'completed'
+            ),
+            (
+                (SELECT dog_id FROM Dogs WHERE name='Tiny'),
+            '2025-06-10 08:30:00',
+            5,
+            'Salty Springs',
+            'open'
+            ),
+            (
+                (SELECT dog_id FROM Dogs WHERE name='Beast'),
+            '2025-08-10 21:00:00',
+            120,
+            'CBD',
+            'cancelled'
+        );
     `);
 
+    
 }
