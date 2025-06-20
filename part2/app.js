1bf046ea40418afe417b4267ebce4ad4b4ff37fa
Harry Glen <harrison.glen@student.adelaide.edu.au>
2025-06-20 06:08:19 +0000

    [automatic commit] 2025-06-20 06:08:19 UTC 103.237.18.6

--- part2/app.js
+++ part2/app.js
@@ -37,7 +37,9 @@ app.post('/login', async (req, res) => {
 
     const user = rows[0];
     const match = await bcrypt.compare(password, user.password_has);
-    
+    if (!match) {
+        return res.send()
+    }
 })
 
 // Routes
