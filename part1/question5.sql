422fd2506a169072869b0a22b6915c04028a44bb
Harry Glen <harrison.glen@student.adelaide.edu.au>
2025-06-20 02:48:53 +0000

    [automatic commit] 2025-06-20 02:48:53 UTC 103.237.18.6

--- part1/question5.sql
+++ part1/question5.sql
@@ -41,7 +41,8 @@ VALUES(
     (SELECT dog_id FROM Dogs WHERE name='Max'),
     '2025-06-10 08:00:00',
     30,
-    'Parklands'
+    'Parklands',
+    ''
     ),
     (
     (SELECT dog_id FROM Dogs WHERE name='name'),
