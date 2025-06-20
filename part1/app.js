2d5995e0311320abf2f9e47f8cf5d553fec4b11d
Harry Glen <harrison.glen@student.adelaide.edu.au>
2025-06-20 03:36:41 +0000

    [automatic commit] 2025-06-20 03:36:41 UTC 103.237.18.6

--- part1/app.js
+++ part1/app.js
@@ -163,6 +163,7 @@ async function main() {
 
     // Start server
     app.listen(3000, () =>{
-        console.log('Server running on http://;')
-    })
+        console.log('Server running on http://localhost:3000');
+    });
+}
 }
