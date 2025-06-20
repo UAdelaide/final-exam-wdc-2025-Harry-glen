05626c75b46580ca03aeccea7766be09dfb96d33
Harry Glen <harrison.glen@student.adelaide.edu.au>
2025-06-20 06:13:30 +0000

    [automatic commit] 2025-06-20 06:13:29 UTC 103.237.18.6

--- part2/app.js
+++ part2/app.js
@@ -57,5 +57,8 @@ const userRoutes = require('./routes/userRoutes');
 app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
 
+// Protect dashbaords
+app.get('')
+
 // Export the app instead of listening here
 module.exports = app;
