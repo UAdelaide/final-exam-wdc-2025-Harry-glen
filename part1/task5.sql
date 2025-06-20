2b893589ebb4bec49000a4bc579f3eefbb63cf9c
Harry Glen <harrison.glen@student.adelaide.edu.au>
2025-06-20 02:25:35 +0000

    [automatic commit] 2025-06-20 02:25:35 UTC 103.237.18.6

--- part1/task5.sql
+++ part1/task5.sql
@@ -1,3 +1,5 @@
 INSERT INTO Users (username, email, password_hash, role)
-VALUES ('alice123', 'alice@example.com', 'hashed123', 'owner'),
+VALUES
+    ('alice123', 'alice@example.com', 'hashed123', 'owner'),
+    ('alice123', 'alice@example.com', 'hashed123', 'owner');
 
